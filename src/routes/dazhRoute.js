7976f11d159028d65b5fe0c1691338f2d39b0a27
RifabAhamed <rifabahamed@gmail.com>
2024-07-10 14:42:38 +0530

    widget routes created and validate has to be done

--- src/routes/dazhRoute.js
+++ src/routes/dazhRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const dazhController = new DazhController();
 
 router.post("/create-dazh", validate(workspaceDEFValidationSchema), authMiddleware ,dazhController.createDazhController);
-router.get("/widget-name-check", authMiddleware ,dazhController.dazhNameCheckController);
+router.get("/dazh-name-check", authMiddleware ,dazhController.dazhNameCheckController);
 router.get("/get-all-dazh", validate(paginationSchema) ,authMiddleware ,dazhController.getAllDazhsPaginatedController);
 router.get("/update-dazh", validate(paginationSchema) ,authMiddleware ,dazhController.updateDazhController);
 
--- src/routes/widgetRoute.js
+++ src/routes/widgetRoute.js
@@ -6,8 +6,9 @@ import WidgetController from "../controllers/WidgetController.js";
 const router = express.Router();
 const widgetController = new WidgetController();
 
-// router.post("/create-def", validate(workspaceDEFValidationSchema), authMiddleware ,defController.createDEFController);
+router.post("/create-widget", validate(workspaceDEFValidationSchema), authMiddleware ,widgetController.createWidgetController);
 router.get("/widget-name-check", authMiddleware ,widgetController.widgetNameCheckController);
-// router.get("/get-all-defs", validate(paginationSchema) ,authMiddleware ,defController.getAllDefsPaginatedController);
+router.get("/get-all-widget", validate(paginationSchema) ,authMiddleware ,widgetController.getAllWidgetsPaginatedController);
+router.get("/update-widget", validate(paginationSchema) ,authMiddleware ,widgetController.updateWidgetController);
 
 export default router;
